e26d168b21b5ca544d274ff1115641543dcc6eb4
wellington <wellingtonslpbi@hotmail.com>
2020-10-06 20:30:54 -0300

    add prettier

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ message: "Ok" });
+app.get('/', (req, res) => {
+  res.json({ message: 'Ok' });
 });
 
 app.listen(3333, () => {
-  console.log("Server is running on port: 3333");
+  console.log('Server is running on port: 3333');
 });
