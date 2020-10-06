1f50911e4cf69600d9dda8ac9287db78784666a6
wellington <wellingtonslpbi@hotmail.com>
2020-10-06 20:54:27 -0300

    add appointment router

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,8 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Ok' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server is running on port: 3333');
