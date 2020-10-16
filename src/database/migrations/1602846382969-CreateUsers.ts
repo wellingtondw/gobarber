79c81b971eca46fea73b77d7b441e3aa5e393031
wellington <wellingtonslpbi@hotmail.com>
2020-10-16 08:38:25 -0300

    create allter provider field to provider id migration

--- src/database/migrations/1602846382969-CreateUsers.ts
+++ src/database/migrations/1602846382969-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1602846382969 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
@@ -42,6 +42,6 @@ export default class CreateUsers1602846382969 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
