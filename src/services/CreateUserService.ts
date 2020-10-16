653f73f07841fa526c5498ae6a4bd5f62ce595e8
wellington <wellingtonslpbi@hotmail.com>
2020-10-16 20:13:29 -0300

    password encryption

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,6 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
 import User from '../models/User';
 
 interface Request {
@@ -19,7 +21,13 @@ class CreateUserService {
       throw new Error('Email already exists.');
     }
 
-    const user = usersRepository.create({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     await usersRepository.save(user);
 
