1f166e233ded8059242bb01baa3483fc208a103e
wellington <wellingtonslpbi@hotmail.com>
2020-10-18 12:05:00 -0300

    create user session

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authUserService = new AuthenticateUserService();
+
+    const { user } = await authUserService.execute({ email, password });
+
+    delete user.password;
+
+    return response.json({ user });
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+});
+
+export default usersRouter;
